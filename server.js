3785b1d86664c90d57e0296555c1d339d6593dc7
Ben Holland <ben.holland@flubit.com>
2014-09-11 14:43:03 +0100

    Added simple expres server

---
+++ server.js
@@ -0,0 +1,26 @@
+var express  = require('express');
+
+var app = express();
+
+app.configure(function() {
+    app.set('port', parseInt(process.env.PORT, 10) || 3000);
+    app.use(express.methodOverride());
+    app.use(express.cookieParser());
+    app.use(express.bodyParser());
+    app.use(express.static(__dirname + '/dist'));
+    // app.use(express.favicon('app/favicon.ico'));
+
+    app.set('view engine', 'ejs'); // set up ejs for templating
+    app.set('views' ,__dirname + '/dist');
+
+    app.use(express.errorHandler({
+        dumpExceptions: true,
+        showStack: true
+    }));
+
+    app.use(app.router);
+});
+
+app.listen(app.get('port'), function(){
+    console.log('Express server listening on port: ' + app.get('port'));
+});
