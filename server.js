8293a72eca1dacdf121e6f7f607f14c52db07e1d
Ben Holland <benholland99@gmail.com>
2014-09-16 11:09:54 +0100

    Added Jenkins endpoint

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-var express  = require('express');
+var http = require('http');
+var express = require('express');
 
 var app = express();
 
@@ -19,6 +20,32 @@ app.configure(function() {
     }));
 
     app.use(app.router);
+});
+
+var jenkinsOptions = {
+    host: 'ci.flubdev.info',
+    port: 8080,
+    path: '/api/json',
+    headers: {
+        'Authorization': 'Basic ' + new Buffer('BenH' + ':' + '729c06d81c39e0a960b60f1793c199b0').toString('base64')
+    }
+};
+
+app.get('/jenkins', function(req, res) {
+
+    http.request(jenkinsOptions, function(response) {
+        var str = '';
+
+        //another chunk of data has been recieved, so append it to `str`
+        response.on('data', function (chunk) {
+            str += chunk;
+        });
+
+        //the whole response has been recieved, so we just print it out here
+        response.on('end', function () {
+            res.send(JSON.parse(str));
+        });
+    }).end();
 });
 
 app.listen(app.get('port'), function(){
